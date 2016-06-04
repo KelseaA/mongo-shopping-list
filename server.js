5cf1812e34d581d9eddbcd01175f95280263aabd
Kelsea <andersonkelsea@gmail.com>
2016-06-04 13:21:36 -0400

    Changed port

--- server.js
+++ server.js
@@ -12,8 +12,11 @@ app.use('*', function(req, res) {
     res.status(404).json({ message: 'Not Found' });
 });
 
-app.listen("8080", function() {
-    console.log('Listening on port 8080');
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+app.listen(port, function() {
 });
 
 exports.app = app;
